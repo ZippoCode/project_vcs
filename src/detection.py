bbcba359942901495daee4996d6c09e12c834b20
Hoang Minh Tri <tri.hm@agiletech.vn>
2020-05-18 17:35:51 +0200

    update

--- src/detection.py
+++ src/detection.py
@@ -3,6 +3,7 @@ import cv2
 
 from matplotlib import pyplot as plt
 
+
 def show_image(images=[], titles=[]):
     fig = plt.figure(figsize=(150, 200))
     nrows = 1
@@ -17,7 +18,7 @@ def show_image(images=[], titles=[]):
         plt.xticks([])
         plt.yticks([])
 
-    #plt.show()
+    # plt.show()
 
 
 def edge_detection(im):
@@ -31,8 +32,10 @@ def edge_detection(im):
 
     # PYR MEAN SHIFT FILTERING
     im = cv2.pyrMeanShiftFiltering(im, sp=2, sr=8, maxLevel=3)
-    images.append(im);
+    images.append(im)
     titles.append('Mean Shift Filtering')
+    cv2.imshow('Mean Shift Filtering', im)
+    cv2.waitKey(0)
 
     # Apply threshould
     hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
@@ -40,6 +43,8 @@ def edge_detection(im):
     # im = cv2.cvtColor(im_threshould, cv2.COLOR_HSV2RGB)
     images.append(im)
     titles.append("Threshould Image")
+    cv2.imshow('Threshould Image', im)
+    cv2.waitKey(0)
 
     """
     # Try to search pixel's values which correspond at the wall and I try to remove these
@@ -53,9 +58,12 @@ def edge_detection(im):
 
     # Canny
     # im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-    im = cv2.Canny(im, threshold1=50, threshold2=150, apertureSize=3, L2gradient=True)
+    im = cv2.Canny(im, threshold1=50, threshold2=150,
+                   apertureSize=3, L2gradient=True)
     images.append(im)
     titles.append('Canny Filtering Image')
+    cv2.imshow('Canny', im)
+    cv2.waitKey(0)
 
     # Lines
     im_lines = np.copy(im_original)
