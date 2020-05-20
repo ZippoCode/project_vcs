ca6a21173b7ed1912e0f54ce5dc19ba1f83d950d
Hoang Minh Tri <tri.hm@agiletech.vn>
2020-05-20 15:53:45 +0200

    update

--- src/recfitication.py
+++ src/recfitication.py
@@ -5,6 +5,7 @@ import math
 from detection import edge_detection
 from parameters import *
 from plotting import plt_images
+import matplotlib.pyplot as plt
 
 
 def recfitication(im):
@@ -15,7 +16,10 @@ def recfitication(im):
     images.append(img)
     titles.append('Original Image')
 
+    # Convert to gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Smooth
     gray = cv2.GaussianBlur(gray, (3, 3), 15)
     images.append(gray)
     titles.append('Gray')
@@ -30,7 +34,6 @@ def recfitication(im):
     images.append(gray)
     titles.append('MedianBlur')
 
-    # Connected Component
     KERNEL_HIGH_PASS_FILTER = np.asarray(
         [[0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [1, 1, 1, 1, 1], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]], np.uint8)
     gray = cv2.erode(gray, KERNEL_HIGH_PASS_FILTER)
@@ -38,15 +41,12 @@ def recfitication(im):
                                     dtype=np.uint8), iterations=DILATE_ITERATIONS)
     gray = cv2.erode(gray, KERNEL_HIGH_PASS_FILTER,
                      iterations=EROSION_ITERATIONS)
-    images.append(gray)
-    titles.append('Erode Dilate')
+    # images.append(gray)
+    # titles.append('Erode Dilate')
 
     # Connected components
     _, labeled_img = cv2.connectedComponentsWithAlgorithm(
         gray, 8, cv2.CV_32S, cv2.CCL_GRANA)
-    images.append(labeled_img)
-    titles.append('Labeled Img 01')
-
     labels = np.unique(labeled_img)
     labels = labels[labels != 0]
     im = np.zeros_like(labeled_img, dtype=np.uint8)
@@ -68,8 +68,16 @@ def recfitication(im):
 
         global_mask = np.clip(
             global_mask + hull_mask, 0, 255)
-    images.append(global_mask)
-    titles.append('Connected components')
+    # images.append(global_mask)
+    # titles.append('Connected components')
+
+    # dst = cv2.cornerHarris(global_mask, 2, 3, 0.04)
+    # # result is dilated for marking the corners, not important
+    # dst = cv2.dilate(dst, None)
+    # # Threshold for an optimal value, it may vary depending on the image.
+    # img[dst > 0.01*dst.max()] = [0, 0, 255]
+    # images.append(img)
+    # titles.append('Detect corner')
 
     # detect corners with the goodFeaturesToTrack function.
     corners = cv2.goodFeaturesToTrack(global_mask, 27, 0.01, 10)
@@ -79,11 +87,12 @@ def recfitication(im):
     # making a circle at each point that we think is a corner.
     for i in corners:
         x, y = i.ravel()
+        # print(x, y)
         cv2.circle(img, (x, y), 3, 255, -1)
     images.append(img)
     titles.append('Detect corner')
 
-    plt_images(images, titles)
+    # plt_images(images, titles)
     return gray
 
 # recfitication('../data/Amalia_image.jpeg')
@@ -97,9 +106,9 @@ for painting in range(len(list_painting)):
     x, y, w, h = list_painting[painting]
     img_crop = im[y:y+h, x:x+w, :]
     gray = recfitication(img_crop)
-    # cv2.imwrite('../output/100_'+str(painting)+'.jpg', img_crop)
-    # drawing_frame = cv2.rectangle(
-    #     im, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imwrite('../output/100_'+str(painting)+'.jpg', img_crop)
+    drawing_frame = cv2.rectangle(
+        im, (x, y), (x + w, y + h), (0, 255, 0), 3)
     # cv2.imshow('crop_img', img_crop)
     # cv2.waitKey()
 
